14c29a0536fb8f56f005067f3d015e1f9d0fbb7f
Anton Metlitsky <ticket1201@gmail.com>
2022-05-23 23:08:24 +0300

    feat: add props data, todolist component impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+
 
 function App() {
+
+    // BLL: business logic layer
+
+    const tasks_1: Array<TaskType> = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS/TS', isDone: true },
+        { id: 3, title: 'React', isDone: false }
+    ]
+
+    const tasks_2: Array<TaskType> = [
+        { id: 4, title: 'Meat', isDone: false },
+        { id: 5, title: 'Fish', isDone: false },
+        { id: 6, title: 'Bear', isDone: false }
+    ]
+
+    //ui
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={'What to learn'} tasks={tasks_1}/>
+            <TodoList title={'What to buy'} tasks={tasks_2}/>
         </div>
     );
 }

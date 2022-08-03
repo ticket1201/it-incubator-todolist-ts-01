c6f0ee5bef62a4748fcb6cd910eca73e261dc70e
Anton Metlitsky <ticket1201@gmail.com>
2022-08-03 22:44:16 +0300

    feat: change import to App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppWithRedux from './AppWithRedux';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './state/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>,
     document.getElementById('root'));
 

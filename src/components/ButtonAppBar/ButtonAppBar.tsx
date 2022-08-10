ae91ed548015f1690ece3f5a81bb98ae423f72ca
Anton Metlitsky <ticket1201@gmail.com>
2022-08-11 00:25:22 +0300

    refactor: del unnecessary typography element, add justifyContent style

--- src/components/ButtonAppBar/ButtonAppBar.tsx
+++ src/components/ButtonAppBar/ButtonAppBar.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -11,7 +10,7 @@ export default function ButtonAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar sx={{ justifyContent: `space-between` }}>
                     <IconButton
                         size="large"
                         edge="start"
@@ -21,9 +20,6 @@ export default function ButtonAppBar() {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>

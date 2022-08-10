49a6104605c0099d62db840ff90351e4b64fb6ef
Anton Metlitsky <ticket1201@gmail.com>
2022-08-10 22:33:39 +0300

    feat: implement App stories

---
+++ src/App.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import App from './App';
+import ReduxStoreProviderDecorator from './state/ReduxStoreProviderDecorator';
+
+export default {
+    title: 'TODOLIST/APP',
+    component: App,
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = () => <App/>;
+
+export const AppStory = Template.bind({})
+

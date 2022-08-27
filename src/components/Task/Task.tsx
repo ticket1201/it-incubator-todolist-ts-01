77c0de66d7897c9542fde8f1bdd1bff4eb63e43e
Anton Metlitsky <ticket1201@gmail.com>
2022-08-27 16:58:20 +0300

    refactor: rework component to new taskReducer data format and thunks

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent, memo} from 'react';
 import {Checkbox, IconButton} from '@mui/material';
 import {EditableSpan} from '../EditableSpan/EditableSpan';
 import {Delete} from '@material-ui/icons';
-import {changeTaskStatusAC, changeTaskTitleAC, removeTaskAC} from '../../state/tasksReducer';
+import {deleteTaskTC, updateTaskTC} from '../../state/tasksReducer';
 import {useDispatch} from 'react-redux';
 import {TaskStatuses, TaskType} from '../../api/todolist-api';
 
@@ -15,15 +15,15 @@ type TaskPropsType = {
 export const Task = memo(({task, todolistID}: TaskPropsType) => {
     const dispatch = useDispatch()
 
-    const onClickHandler = () => dispatch(removeTaskAC(task.id, todolistID))
+    const onClickHandler = () => dispatch(deleteTaskTC(todolistID, task.id))
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        let newIsDoneValue = e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New;
-        dispatch(changeTaskStatusAC(task.id, newIsDoneValue, todolistID));
+        let status = e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New;
+        dispatch(updateTaskTC(todolistID, task.id, {status}));
     }
 
-    const onTitleChangeHandler = (newValue: string) => {
-        dispatch(changeTaskTitleAC(task.id, newValue, todolistID));
+    const onTitleChangeHandler = (title: string) => {
+        dispatch(updateTaskTC(todolistID, task.id, {title}));
     }
 
     return (

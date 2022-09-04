950814c683f84eebd251d6ddbdd5a54ffe8f5351
Anton Metlitsky <ticket1201@gmail.com>
2022-09-04 22:29:53 +0300

    feat: impl app-reducer

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,33 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: null as null | string
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: appActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return state
+    }
+}
+
+export const setAppStatusAC = (status:RequestStatusType) => ({
+    type: 'APP/SET-STATUS',
+    status
+} as const)
+
+export const setAppErrorAC = (error:null | string) => ({
+    type: 'APP/SET-ERROR',
+    error
+} as const)
+
+export type appActionsType =
+    |  ReturnType<typeof setAppStatusAC>
+    |  ReturnType<typeof setAppErrorAC>

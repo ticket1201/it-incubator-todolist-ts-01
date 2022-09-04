c75e8cc9d582cac6068808bdf5a34f9f533a08ef
Anton Metlitsky <ticket1201@gmail.com>
2022-09-04 22:21:49 +0300

    feat: rework AddItemForm component. Add message length check, disabled status

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,13 +1,18 @@
 import React, {ChangeEvent, KeyboardEvent, memo, useState} from 'react';
-import {Button, TextField} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
+    label: string
+    disabled?: boolean
 }
 
 export const AddItemForm = memo((props: AddItemFormPropsType) => {
     let [title, setTitle] = useState('')
     let [error, setError] = useState<string | null>(null)
+
+    const isBtnDisabled = title.length > 100;
 
     const addItem = () => {
         if (title.trim() !== '') {
@@ -19,11 +24,18 @@ export const AddItemForm = memo((props: AddItemFormPropsType) => {
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.value.length > 100) {
+            setError('Maximum length exceeded')
+        } else {
+            if (error) {
+                setError('')
+            }
+        }
         setTitle(e.currentTarget.value)
     }
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(error) setError(null);
+        if (error) setError(null);
         if (e.key === 'Enter') {
             addItem();
         }
@@ -33,13 +45,14 @@ export const AddItemForm = memo((props: AddItemFormPropsType) => {
         <TextField value={title}
                    variant={'outlined'}
                    size={'small'}
-                   label={!!error ? 'Title is required' : 'Text'}
+                   label={!!error ? error : props.label}
                    error={!!error}
                    onChange={onChangeHandler}
                    onKeyPress={onKeyPressHandler}
                    style={{marginRight: '10px'}}
         />
         <Button
+            disabled={isBtnDisabled || props.disabled}
             onClick={addItem}
             variant={'contained'}
             style={{maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px',}}

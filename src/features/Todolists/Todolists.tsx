2f46b1750cb1b938a04382a27dcf9e657d063de8
Anton Metlitsky <ticket1201@gmail.com>
2022-09-04 22:39:26 +0300

    feat: add AddItemForm to component, rename Thunk for fetch todolists

--- src/features/Todolists/Todolists.tsx
+++ src/features/Todolists/Todolists.tsx
@@ -1,30 +1,40 @@
-import React, {useEffect} from 'react';
-import {Grid} from '@mui/material';
+import React, {useCallback, useEffect} from 'react';
+import Grid from '@mui/material/Grid';
 import {Todolist} from './Todolist/TodoList';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../app/store';
-import {getTodolistsT, TodolistDomainType} from './todolistReducer';
+import {addTodolistsTC, fetchTodolistsT, TodolistDomainType} from './todolistReducer';
+import {AddItemForm} from '../../components/AddItemForm/AddItemForm';
 
 const Todolists = () => {
     const dispatch = useDispatch()
     const todoLists = useSelector<AppRootStateType, TodolistDomainType[]>(state => state.todolists)
+    const addTodolist = useCallback((title: string) => {
+        dispatch(addTodolistsTC(title))
+    }, [dispatch])
+
 
     useEffect(() => {
-        dispatch(getTodolistsT)
+        dispatch(fetchTodolistsT)
     }, [dispatch])
 
     return (
-        <Grid container spacing={5} columns={3} justifyContent={'space-evenly'}>
-            {
-                todoLists.map(tl => {
-                    return <Grid item lg={1} key={tl.id}>
-                        <Todolist
-                            todolist={tl}
-                        />
-                    </Grid>
-                })
-            }
-        </Grid>
+        <>
+            <Grid container style={{margin: '30px'}}>
+                <AddItemForm addItem={addTodolist} label={'Add todolist'}/>
+            </Grid>
+            <Grid container spacing={5} columns={3} justifyContent={'space-evenly'}>
+                {
+                    todoLists.map(tl => {
+                        return <Grid item lg={1} key={tl.id}>
+                            <Todolist
+                                todolist={tl}
+                            />
+                        </Grid>
+                    })
+                }
+            </Grid>
+        </>
     );
 };
 

3cb718efebbb7e53fa39bd64a55543f102b68401
Anton Metlitsky <ticket1201@gmail.com>
2022-09-04 22:35:32 +0300

    feat: add app reducer to store, add useAppSelector (useSelector with types), add store prop to window object

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,14 +2,21 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {todolistReducer} from '../features/Todolists/todolistReducer';
 import {tasksReducer} from '../features/Todolists/tasksReducer';
 import thunk from 'redux-thunk';
+import {TypedUseSelectorHook, useSelector} from 'react-redux';
+import {appReducer} from './app-reducer';
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
 
 const rootReducer = combineReducers({
     todolists: todolistReducer,
-    tasks: tasksReducer
+    tasks: tasksReducer,
+    app: appReducer
 })
 
 
-
 export const store = createStore(rootReducer, applyMiddleware(thunk))
+export type AppRootStateType = ReturnType<typeof rootReducer>
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+
+// @ts-ignore
+window.store = store;

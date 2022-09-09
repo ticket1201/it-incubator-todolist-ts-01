ee66f1d6314aa3cc358b1342f5f4bbcc7afeec81
Anton Metlitsky <ticket1201@gmail.com>
2022-09-09 17:11:42 +0300

    feat: add isInitialized functionality, routes, className to container

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useAppSelector} from './store';
 import './App.css';
 import ButtonAppBar from '../components/ButtonAppBar/ButtonAppBar';
 import Todolists from '../features/Todolists/Todolists';
 import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
+import CircularProgress from '@mui/material/CircularProgress';
 import {CustomizedSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
+import {initAppTC} from './app-reducer';
+import {Login} from '../features/Login/Login';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 
 
 function App() {
+    let dispatch = useDispatch()
+    const isInitialized = useAppSelector(state => state.app.isInitialized)
     const status = useAppSelector( state => state.app.status)
+
+
+    useEffect(()=>{
+        dispatch(initAppTC())
+    }, [dispatch])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%', height: '100vh'}}>
+            <CircularProgress/>
+        </div>
+    }
 
     return (
         <div className="App">
             <ButtonAppBar/>
             <CustomizedSnackbar/>
             {status === 'loading' &&  <LinearProgress color="secondary"/>}
-            <Container fixed>
-                <Todolists/>
+            <Container fixed className={'main'}>
+               <Routes>
+                   <Route path={'/'} element={<Todolists/>}/>
+                   <Route path={'/login'} element={<Login/>}/>
+                   <Route path='*' element={<Navigate to={'/'}/>}/>
+               </Routes>
             </Container>
-
         </div>
     );
 }

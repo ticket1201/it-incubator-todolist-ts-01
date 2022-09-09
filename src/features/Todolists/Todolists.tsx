678a91d68aa36ce6d58b708dbc7e8c6d636e9e05
Anton Metlitsky <ticket1201@gmail.com>
2022-09-09 17:14:55 +0300

    feat: replace useSelector with typed hook, add isLoggined condition check, redirect to login page if not loggined

--- src/features/Todolists/Todolists.tsx
+++ src/features/Todolists/Todolists.tsx
@@ -1,26 +1,37 @@
 import React, {useCallback, useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import {Todolist} from './Todolist/TodoList';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppRootStateType} from '../../app/store';
-import {addTodolistsTC, fetchTodolistsT, TodolistDomainType} from './todolistReducer';
+import {useDispatch} from 'react-redux';
+import {useAppSelector} from '../../app/store';
+import {addTodolistsTC, fetchTodolistsT} from './todolistReducer';
 import {AddItemForm} from '../../components/AddItemForm/AddItemForm';
+import {Navigate} from 'react-router-dom';
 
 const Todolists = () => {
     const dispatch = useDispatch()
-    const todoLists = useSelector<AppRootStateType, TodolistDomainType[]>(state => state.todolists)
+    const todoLists = useAppSelector(state => state.todolists)
+    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
     const addTodolist = useCallback((title: string) => {
         dispatch(addTodolistsTC(title))
     }, [dispatch])
 
 
+
     useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
         dispatch(fetchTodolistsT)
-    }, [dispatch])
+    }, [dispatch, isLoggedIn])
+
+
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'}/>
+    }
 
     return (
         <>
-            <Grid container style={{margin: '30px'}}>
+            <Grid container className={'addTodo'}>
                 <AddItemForm addItem={addTodolist} label={'Add todolist'}/>
             </Grid>
             <Grid container spacing={5} columns={3} justifyContent={'space-evenly'}>

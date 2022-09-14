5a71aa4cd9fcdc1acf82fef91ee3c43c952ff879
Anton Metlitsky <ticket1201@gmail.com>
2022-09-14 20:10:23 +0300

    refactor: rework create store with configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import {todoACType, todolistReducer} from '../features/Todolists/todolistReducer';
 import {tasksACType, tasksReducer} from '../features/Todolists/tasksReducer';
-import {ThunkAction} from 'redux-thunk';
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {appActionsType, appReducer} from './app-reducer';
 import {authActionsType, authReducer} from '../features/Login/authReducer';
 import {configureStore} from '@reduxjs/toolkit';
@@ -15,15 +15,19 @@ const rootReducer = combineReducers({
 })
 
 
+//export const store = createStore(rootReducer, applyMiddleware(thunk))
 export const store = configureStore({
     reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
 })
 
 export type RootStateType = ReturnType<typeof store.getState>
-export type RootDispatchType = typeof store.dispatch
+// export type RootDispatchType = typeof store.dispatch
 
 
 export type RootActionsType = todoACType | tasksACType | appActionsType | authActionsType
+
+export type RootDispatchType = ThunkDispatch<RootStateType, unknown, RootActionsType>
 export type RootThunkType<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, RootActionsType>
 
 

9198b761f295247dc2f13fe9718d772deb5c9dcb
Anton Metlitsky <ticket1201@gmail.com>
2022-09-23 14:14:15 +0300

    feat: add storybookUrl prop to test

--- src/stories/Storyshots.test.js
+++ src/stories/Storyshots.test.js
@@ -3,19 +3,19 @@ import path from 'path';
 import initStoryshots from '@storybook/addon-storyshots';
 
 // The required import from the @storybook/addon-storyshots-puppeteer addon
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer';
 
 // Function to customize the snapshot location
-const getMatchOptions = ({ context: { fileName } }) => {
+const getMatchOptions = ({context: {fileName}}) => {
     // Generates a custom path based on the file name and the custom directory.
     const snapshotPath = path.join(path.dirname(fileName), '_snap_');
-    return { customSnapshotsDir: snapshotPath };
+    return {customSnapshotsDir: snapshotPath};
 };
 
 initStoryshots({
     // your own configuration
     test: imageSnapshot({
         // invoke the function above here
-        getMatchOptions,
+        getMatchOptions, storybookUrl: 'http://localhost:9009/'
     }),
 });

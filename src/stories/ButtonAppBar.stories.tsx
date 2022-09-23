f321d0445dc23c552722f1b454425549a54c6a0d
Anton Metlitsky <ticket1201@gmail.com>
2022-09-23 14:14:27 +0300

    feat: add Provider to storie

--- src/stories/ButtonAppBar.stories.tsx
+++ src/stories/ButtonAppBar.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 import ButtonAppBar from '../components/ButtonAppBar/ButtonAppBar';
+import ReduxStoreProviderDecorator from './decorators/ReduxStoreProviderDecorator';
 
 export default {
     title: 'TODOLIST/ButtonAppBar',
-    component: ButtonAppBar
+    component: ButtonAppBar,
+    decorators: [ReduxStoreProviderDecorator]
 } as ComponentMeta<typeof ButtonAppBar>;
 
 
